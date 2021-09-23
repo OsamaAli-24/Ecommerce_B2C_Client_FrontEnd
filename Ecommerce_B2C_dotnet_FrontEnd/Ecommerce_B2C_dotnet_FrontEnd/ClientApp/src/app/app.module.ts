87b939648f9bd55f96b4aa3c808898be5b6a4566
ali <raoalihamid810@gmail.com>
2021-09-24 03:41:37 +0500

    Login Front end And dashboard components

--- Ecommerce_B2C_dotnet_FrontEnd/Ecommerce_B2C_dotnet_FrontEnd/ClientApp/src/app/app.module.ts
+++ Ecommerce_B2C_dotnet_FrontEnd/Ecommerce_B2C_dotnet_FrontEnd/ClientApp/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, DashboardComponent],
   imports: [
     BrowserModule,
     FormsModule,
